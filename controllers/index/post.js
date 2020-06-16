2c12058430d19e9d4a491465335ee27078ae235d
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-06-16 22:21:47 +0300

    Small updates on UX

--- controllers/index/post.js
+++ controllers/index/post.js
@@ -1,16 +1,16 @@
 const Application = require('../../models/application/Application');
 
 module.exports = (req, res) => {
-  if (!req.body || !req.body.name || !req.body.title || !req.body.area || !req.body.company_name || !req.body.phone || !req.body.link)
+  if (!req.body || !req.body.user_name || !req.body.user_title || !req.body.business_area || !req.body.company_name || !req.body.company_phone || !req.body.company_link)
     return res.redirect('/');
 
   const newApplicationData = {
-    name: req.body.name,
-    title: req.body.title,
-    area: req.body.area,
+    name: req.body.user_name,
+    title: req.body.user_title,
+    area: req.body.business_area,
     company_name: req.body.company_name,
-    phone: req.body.phone,
-    link: req.body.link
+    phone: req.body.company_phone,
+    link: req.body.company_link
   };
 
   const newApplication = new Application(newApplicationData);
