a203be6e353deeb7bba9ba83744f84fd01c843b6
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-09-21 12:12:37 +0300

    Fix /admin/submitions/approve_all GET route

--- controllers/admin/submitions/approve_all/get.js
+++ controllers/admin/submitions/approve_all/get.js
@@ -11,51 +11,54 @@ module.exports = (req, res) => {
   Campaign.findById(mongoose.Types.ObjectId(req.query.id), (err, campaign) => {
     if (err) return res.redirect('/admin');
 
+    let submitions = campaign.submitions;
+    let accepted_submitions = campaign.accepted_submitions;
+
     async.times(
       Math.min(campaign.submitions.length, 50),
       (time, next) => {
-        const user_id = campaign.submitions[time].user_id;
-        const submitions = campaign.submitions.filter(sub => !sub.user_id || sub.user_id.toString() != user_id);
+        if (!campaign.submitions[time].user_id) {
+          submitions = submitions.filter((sub, i) => i != time);
+          return next(null);
+        }
 
-        if (submitions.length == campaign.submitions.length)
-          return next(true);
-    
+        const user_id = campaign.submitions[time].user_id;
+
+        submitions = submitions.filter(sub => sub.user_id && sub.user_id.toString() != user_id);
+        accepted_submitions.push(user_id);
+
         User.findById(mongoose.Types.ObjectId(user_id), (err, user) => {
-          if (err || !user) return next(err || true);
+          if (err) return next(err);
     
-          Campaign.findByIdAndUpdate(mongoose.Types.ObjectId(req.query.id), {
-            $set: {
-              submitions
+          const campaign_status = user.campaign_status;
+          campaign_status[req.query.id] = "approved";
+
+          User.findByIdAndUpdate(mongoose.Types.ObjectId(user_id), {
+            $set: { campaign_status },
+            $inc: {
+              credit: user.paid_campaigns.includes(req.query.id) ? 0 : campaign.price
             },
             $push: {
-              accepted_submitions: user._id.toString()
+              paid_campaigns: req.query.id.toString()
             }
           }, {}, err => {
             if (err) return next(err);
-
-            const campaign_status = user.campaign_status;
-            campaign_status[req.query.id] = "approved";
-
-            User.findByIdAndUpdate(mongoose.Types.ObjectId(user_id), {
-              $set: { campaign_status },
-              $inc: {
-                credit: user.paid_campaigns.includes(req.query.id) ? 0 : campaign.price
-              },
-              $push: {
-                paid_campaigns: req.query.id.toString()
-              }
-            }, {}, (err, user) => {
-              if (err || !user) return res.redirect('/admin');
     
-              return next(false);
-            });
+            return next(null);
           });
         });
       },
       err => {
         if (err) return res.redirect('/admin');
 
-        return res.redirect('/admin/submitions?id=' + req.query.id);
+        Campaign.findByIdAndUpdate(mongoose.Types.ObjectId(req.query.id), {$set: {
+          submitions,
+          accepted_submitions
+        }}, {}, err => {
+          if (err) return res.redirect('/admin');
+
+          return res.redirect('/admin/submitions?id=' + req.query.id + '&version=1.0');
+        });
       }
     );
   });
