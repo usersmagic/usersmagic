42cfc3a9521d00a1c6f5d5a97266b7ce8c245da3
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-09-24 13:17:23 +0300

    -

--- controllers/admin/update/get.js
+++ controllers/admin/update/get.js
@@ -2,33 +2,31 @@ const async = require('async');
 const mongoose = require('mongoose');
 
 const User = require('../../../models/user/User');
+const Campaign = require('../../../models/campaign/Campaign');
 
 module.exports = (req, res) => {
   if (!req.query || !req.query.updates)
     return res.redirect('/');
 
-  User.find({ $and: [
-    { "information.5f613e90dd11cf00161bc632": { $exists: true } },
-    { "information.5f613eefdd11cf00161bc633": { $exists: true } },
-    { "information.5f613f44dd11cf00161bc634": { $exists: true } },
-    { "information.5f613f98dd11cf00161bc636": { $exists: true } },
-    { "information.5f613ff7dd11cf00161bc637": { $exists: true } },
-  ] }, (err, users) => {
-    if (err) console.log(err);
+  Campaign.find({}, (err, campaigns) => {
     if (err) return res.redirect('/');
 
     async.times(
-      users.length,
+      campaigns.length,
       (time, next) => {
-        User.findByIdAndUpdate(mongoose.Types.ObjectId(users[time]._id), {$inc: {
-          credit: 1
-        }}, {}, err => next(err));
+        Campaign.findById(mongoose.Types.ObjectId(campaigns[time]._id), (err, campaign) => {
+          if (err) return next(err);
+
+          Campaign.findByIdAndUpdate(mongoose.Types.ObjectId(campaign._id), {$set: {
+            submitions: campaign.submitions.filter(each => each.user_id)
+          }}, {}, err => next(err));
+        });
       },
       err => {
         if (err) return res.redirect('/');
 
         return res.redirect('/admin');
       }
-    );
+    )
   });
 }
