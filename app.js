43b02e99e2c4d430b1381e69ef3e2eb12ff98c7e
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-09-24 23:27:07 +0300

    Change to use workers

--- app.js
+++ app.js
@@ -1,73 +1,96 @@
 const express = require('express');
+const cluster = require('cluster');
 const http = require('http');
 const path = require('path');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');  
 const dotenv = require('dotenv');
 const favicon = require('serve-favicon');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const expressSession = require('express-session');
 const i18n = require('i18n');
 
-const app = express();
-const server = http.createServer(app);
+const MongoStore = require('connect-mongo')(session);
 
-i18n.configure({
-  locales:['tr', 'en', 'de', 'es', 'fr'],
-  directory: __dirname + '/translations',
-  queryParameter: 'lang',
-  defaultLocale: 'en'
-});
+const numCPUs = process.env.WEB_CONCURRENCY || require('os').cpus().length;
 
-dotenv.config({ path: path.join(__dirname, ".env") });
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-const PORT = process.env.PORT || 3000;
-const mongoUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/usersmagic";
+  for (let i = 0; i < numCPUs; i++)
+  cluster.fork();
 
-const indexRouteController = require('./routes/indexRoute');
-const adminRouteController = require('./routes/adminRoute');
-const authRouteController = require('./routes/authRoute');
-const campaignsRouteController = require('./routes/campaignsRoute');
-const profileRouteController = require('./routes/profileRoute');
-const testRouteController = require('./routes/testRoute');
-const historyRouteController = require('./routes/historyRoute');
-const brandRouteController = require('./routes/brandRoute');
-const agreementRouteController = require('./routes/agreementRoute');
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork(); // Create a New Worker, If Worker is Dead
+  });
+} else {
+  const app = express();
+  const server = http.createServer(app);
+  
+  i18n.configure({
+    locales:['tr', 'en', 'de', 'es', 'fr'],
+    directory: __dirname + '/translations',
+    queryParameter: 'lang',
+    defaultLocale: 'en'
+  });
+  
+  dotenv.config({ path: path.join(__dirname, ".env") });
+  
+  const PORT = process.env.PORT || 3000;
+  const mongoUri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/usersmagic";
+  
+  const indexRouteController = require('./routes/indexRoute');
+  const adminRouteController = require('./routes/adminRoute');
+  const authRouteController = require('./routes/authRoute');
+  const campaignsRouteController = require('./routes/campaignsRoute');
+  const profileRouteController = require('./routes/profileRoute');
+  const testRouteController = require('./routes/testRoute');
+  const historyRouteController = require('./routes/historyRoute');
+  const brandRouteController = require('./routes/brandRoute');
+  const agreementRouteController = require('./routes/agreementRoute');
+  
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "pug");
+  
+  mongoose.connect(mongoUri, { useNewUrlParser: true, auto_reconnect: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
+  
+  app.use(express.static(path.join(__dirname, "public")));
+  
+  app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+  
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  
+  const sessionOptions = session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false
+    },
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    })
+  });
+  
+  app.use(sessionOptions);
+  app.use(cookieParser());
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
-mongoose.connect(mongoUri, { useNewUrlParser: true, auto_reconnect: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-const session = expressSession({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: false
-  }
-});
-
-app.use(session);
-app.use(i18n.init);
-
-app.use('/', indexRouteController);
-app.use('/admin', adminRouteController);
-app.use('/auth', authRouteController);
-app.use('/campaigns', campaignsRouteController);
-app.use('/profile', profileRouteController);
-app.use('/test', testRouteController);
-app.use('/history', historyRouteController);
-// app.use('/brand', brandRouteController);
-app.use('/agreement', agreementRouteController);
-
-server.listen(PORT, () => {
-  console.log(`Server is on port ${PORT}`);
-});
+  app.use(i18n.init);
+  
+  app.use('/', indexRouteController);
+  app.use('/admin', adminRouteController);
+  app.use('/auth', authRouteController);
+  app.use('/campaigns', campaignsRouteController);
+  app.use('/profile', profileRouteController);
+  app.use('/test', testRouteController);
+  app.use('/history', historyRouteController);
+  // app.use('/brand', brandRouteController);
+  app.use('/agreement', agreementRouteController);
+  
+  server.listen(PORT, () => {
+    console.log(`Server is on port ${PORT} as Worker ${cluster.worker.id} running @ process ${cluster.worker.process.pid}`);
+  });
+}
 
