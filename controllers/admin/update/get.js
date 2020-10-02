02207f5822b4cdce275c0c7a3735ad7922aa60d5
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-10-02 15:38:52 +0300

    -

--- controllers/admin/update/get.js
+++ controllers/admin/update/get.js
@@ -10,17 +10,15 @@ module.exports = (req, res) => {
   if (!req.query || !req.query.updates)
     return res.redirect('/');
 
-  User.find({
-    completed: true,
-    country: "tr"
-  }, (err, users) => {
-    if (err) return res.json({err});
+  User.find({$and: [
+    {"information.5f74c3442b4e90001c72b747": {$ne: null}},
+    {"information.5f74c3442b4e90001c72b747": {$ne: "Lisanslı sporcu değilim"}},
+    {gender: "kadın"},
+    {birth_date: {$gte: 2001}},
+    {birth_date: {$lte: 2007}},
+  ]}, (err, users) => {
+    if (err) return res.redirect('/');
 
-    sendMail({
-      emailList: users.map(user => user.email) ,
-    }, 'completeCampaign', err => {
-      if (err) return res.json({err});
-      res.redirect('/admin');
-    });
-  });
+    return res.json({count: users.length, users});
+  })
 }
