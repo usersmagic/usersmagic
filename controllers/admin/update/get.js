01f6b5ffac99461fbfa880bf8310daacca62acb3
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-10-16 22:54:37 +0300

    -

--- controllers/admin/update/get.js
+++ controllers/admin/update/get.js
@@ -2,30 +2,32 @@ const async = require('async');
 const mongoose = require('mongoose');
 
 const User = require('../../../models/user/User');
+const Campaign = require('../../../models/campaign/Campaign');
 
 const sendMail = require('../../../utils/sendMail');
 
 module.exports = (req, res) => {
-  if (!req.query || !req.query.updates || req.query.updates != "new")
+  if (!req.query || !req.query.updates)
     return res.redirect('/');
 
-  User.find({
-    private_campaigns: "5f89e8f6673ead001c9ed286"
-  }, (err, users) => {
+  User.find({$and: [
+    {"information.5f74c3442b4e90001c72b747": {$ne: null}},
+    {"information.5f74c3442b4e90001c72b747": {$ne: "Lisanslı sporcu değilim"}},
+    {gender: "kadın"},
+    {birth_year: {$gte: 2001}},
+    {birth_year: {$lte: 2007}},
+  ]}, (err, users) => {
     if (err) return res.redirect('/');
 
-    async.times(
-      users.length,
-      (time, next) => {
-        User.findByIdAndUpdate(mongoose.Types.ObjectId(users[time]._id), {$pull: {
-          private_campaigns: "5f89e8f6673ead001c9ed286"
-        }}, err => next(err));
-      },
-      err => {
-        if (err) return res.redirect('/');
-
-        return res.redirect('/admin');
-      }
-    )
-  })
+    return res.json({
+      count: users.length,
+      users: users.map(user => {
+        return {
+          name: user.name,
+          email: user.email,
+          phone: user.phone
+        };
+      })
+    });
+  });
 }
