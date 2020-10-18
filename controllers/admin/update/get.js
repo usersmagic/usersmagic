b8d9d1c6cfd0a1747a69498c2d1600f64b6ba938
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-10-18 22:37:43 +0300

    See campaign results

--- controllers/admin/update/get.js
+++ controllers/admin/update/get.js
@@ -10,17 +10,37 @@ module.exports = (req, res) => {
   if (!req.query || !req.query.updates || req.query.updates != "new")
     return res.redirect('/');
 
-  User.find({
-    completed: true
-  }, (err, users) => {
-    if (err) return res.redirect('/');
+  const cities = ["Istanbul", "Bursa", "Ankara"]
+  const field1 = "information.5f8628093b0921001cfa8b3d";
+  const field2 = "information.5f86285c3b0921001cfa8b3e";
 
-    sendMail({
-      emailList: users.map(user => user.email)
-    }, "newCampaigns", err => {
-      if (err) return res.redirect('/');
+  User.find({$and: [
+    {city: {$in: cities}},
+    {[field1]: "Evet"},
+    {[field2]: "Evet"}
+  ]}, (err, users1) => {
+    if (err) return res.redirect('/admin');
 
-      return res.redirect('/admin');
-    })
-  })
+    User.find({$and: [
+      {city: {$in: cities}},
+      {[field1]: "Evet"},
+      {[field2]: "Hayır"}
+    ]}, (err, users2) => {
+      if (err) return res.redirect('/admin');
+  
+      User.find({$and: [
+        {city: {$in: cities}},
+        {[field1]: "Hayır"},
+        {[field2]: "Evet"}
+      ]}, (err, users3) => {
+        if (err) return res.redirect('/admin');
+    
+        return res.json({
+          "ikisi": users1.map(user => {return {email: user.email, phone: user.phone}}),
+          "25-30": users2.map(user => {return {email: user.email, phone: user.phone}}),
+          "30-40": users3.map(user => {return {email: user.email, phone: user.phone}}),
+        });
+      });
+    });
+  });
 }
