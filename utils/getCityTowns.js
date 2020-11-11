60d46d8fff32b85bb2282e51b0bc6bf22f6d312c
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-11-11 12:13:44 +0300

    -

--- utils/getCityTowns.js
+++ utils/getCityTowns.js
@@ -3,10 +3,12 @@ const towns = [{"il_id":"1","name":"ALADAĞ"}, {"il_id":"1","name":"CEYHAN"}, {"
 const cities = ['adana', 'adıyaman', 'afyonkarahisar', 'ağrı', 'amasya', 'ankara', 'antalya', 'artvin', 'aydın', 'balıkesir', 'bilecik', 'bingöl', 'bitlis', 'bolu', 'burdur', 'bursa', 'çanakkale', 'çankırı', 'çorum', 'denizli', 'diyarbakır', 'edirne', 'elazığ', 'erzincan', 'erzurum', 'eskişehir', 'gaziantep', 'giresun', 'gümüşhane', 'hakkari', 'hatay', 'ısparta', 'mersin', 'istanbul', 'izmir', 'kars', 'kastamonu', 'kayseri', 'kırklareli', 'kırşehir', 'kocaeli', 'konya', 'kütahya', 'malatya', 'manisa', 'kahramanmaraş', 'mardin', 'muğla', 'muş', 'nevşehir', 'niğde', 'ordu', 'rize', 'sakarya', 'samsun', 'siirt', 'sinop', 'sivas', 'tekirdağ', 'tokat', 'trabzon', 'tunceli', 'şanlıurfa', 'uşak', 'van', 'yozgat', 'zonguldak', 'aksaray', 'bayburt', 'karaman', 'kırıkkale', 'batman', 'şırnak', 'bartın', 'ardahan', 'ığdır', 'yalova', 'karabük', 'kilis', 'osmaniye', 'düzce'];
 
 const toLocaleLowerCaseTr = (str) => {
+  str = str.toLocaleLowerCase();
+  
   for (let i = 0; i < str.length; i++)
-    str[i] = (str[i] == 'İ' ? 'i' : str[i]);
+    str[i] = (str[i] == 'i̇' ? 'i' : str[i]);
 
-  return str.toLocaleLowerCase();
+  return str;
 }
 
 module.exports = (city) => {
