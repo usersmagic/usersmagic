2f1c4554e385a8319ba5041585fadd67143b5446
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2020-11-27 17:27:17 +0300

    Fix errors on /admin/private_campaigns/submitions/reject POST route.

--- controllers/admin/private_campaigns/submitions/reject/post.js
+++ controllers/admin/private_campaigns/submitions/reject/post.js
@@ -5,7 +5,7 @@ const Submition = require('../../../../../models/submition/Submition');
 const User = require('../../../../../models/user/User');
 
 module.exports = (req, res) => {
-  if (!req.query || !req.query.id || !req.body || !req.body.reason)
+  if (!req.query || !req.query.id || !req.body || !req.body.reason || !req.body.reason.length)
     return res.redirect('/admin');
 
   Submition.findByIdAndUpdate(mongoose.Types.ObjectId(req.query.id), {
@@ -27,7 +27,7 @@ module.exports = (req, res) => {
       }}, {}, err => {
         if (err) return res.redirect('/admin');
 
-        return res.redirect('/admin/submitions?id=' + submition.campaign_id);
+        return res.redirect('/admin/private_campaigns/submitions?id=' + submition.campaign_id);
       });
     });
   });
